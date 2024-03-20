239ce67f97e56baebaf8d4eb4c2c53008f4f4a20
gourongtao <49665175+gourongtao@users.noreply.github.com>
2024-03-20 08:24:34 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,11 +19,11 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=28467050&pid=6865e327ae488f31c3aab791ab225015&tid=a6597e1eb65d75fccc1b5267edf86fa9", 
+    "https://www.maimemo.com/share/page?uid=28467050&pid=d9f3ebf1178e045be56fceee2ec7b562&tid=6bfbddac101b4720cc4d06d2ac6814f0", 
     "https://www.maimemo.com/share/page?uid=28467050&pid=13674b834b884a0e0e20b6c547fe72ec&tid=3df1c027b5e2648b3ff064445376cd02", 
 ]
   /*
-   * 生成随机数字d
+   * 生成随机数字u
    * @param {number} min 最小值（包含）
    * @param {number} max 最大值（不包含）
    */
